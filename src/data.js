f684b0b1aa6b581fe8fcaca51cbd34ea82c8668e
Your Name <your.email@example.com>
2026-04-03 16:17:11 +0700

    new

--- src/data.js
+++ src/data.js
@@ -114,7 +114,7 @@ export const projects = [
     image: "E_leave.jpg",
     tags: ["React", "Node.js", "Full Stack", "RBAC"],
     github: "https://github.com/rujipob1-png/intern_project_demo",
-    demo: "intern-project-demo.vercel.app",    docs: "/\u0e04\u0e39\u0e48\u0e21\u0e37\u0e2d\u0e01\u0e32\u0e23\u0e43\u0e0a\u0e49\u0e07\u0e32\u0e19\u0e23\u0e30\u0e1a\u0e1a\u0e01\u0e32\u0e23\u0e25\u0e32\u0e2d\u0e34\u0e40\u0e25\u0e47\u0e01\u0e17\u0e23\u0e2d\u0e19\u0e34\u0e01\u0e2a\u0e4c.pdf",    featured: true,
+    demo: "https://intern-project-demo.vercel.app",    docs: "/\u0e04\u0e39\u0e48\u0e21\u0e37\u0e2d\u0e01\u0e32\u0e23\u0e43\u0e0a\u0e49\u0e07\u0e32\u0e19\u0e23\u0e30\u0e1a\u0e1a\u0e01\u0e32\u0e23\u0e25\u0e32\u0e2d\u0e34\u0e40\u0e25\u0e47\u0e01\u0e17\u0e23\u0e2d\u0e19\u0e34\u0e01\u0e2a\u0e4c.pdf",    featured: true,
   },
   {
     title: "B2B Chinese Food Ingredient Supply Management System",
